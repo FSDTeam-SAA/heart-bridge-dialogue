1eabb786ceadc282b5fdc60181caf6bbf4f01964
zihadulIslam2 <zihadul708@gmail.com>
2025-04-11 18:16:28 +0600

    Update Firebase configuration with new project credentials

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,27 +2,28 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
+
+
 // const firebaseConfig = {
-//   apiKey: 'AIzaSyDif8kxDtonmn-nhWM-7kF7i1CRcreoMSM',
-//   authDomain: 'heartbridge-dialogue-57f03.firebaseapp.com',
-//   projectId: 'heartbridge-dialogue-57f03',
-//   storageBucket: 'heartbridge-dialogue-57f03.appspot.com', 
-//   messagingSenderId: '520243577506',
-//   appId: '1:520243577506:web:8bde75e18fff519f484dfb',
-//   measurementId: 'G-E09340HYGQ',
-//   databaseURL: 'https://heartbridge-dialogue-57f03-default-rtdb.firebaseio.com', 
+//   apiKey: 'AIzaSyAzCCwY054FfGVhhO1rLAAJTTY298nY67s',
+//   authDomain: 'my-relationship-ai-1.firebaseapp.com',
+//   projectId: 'my-relationship-ai-1',
+//   storageBucket: 'my-relationship-ai-1.firebasestorage.app',
+//   messagingSenderId: '621812643059',
+//   appId: '1:621812643059:web:ec87d0f92b9addf424b386',
+//   measurementId: 'G-T6MJJZN566',
+//   databaseURL:'https://my-relationship-ai-1-default-rtdb.firebaseio.com/'
 // }
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAzCCwY054FfGVhhO1rLAAJTTY298nY67s',
-  authDomain: 'my-relationship-ai-1.firebaseapp.com',
-  projectId: 'my-relationship-ai-1',
-  storageBucket: 'my-relationship-ai-1.firebasestorage.app',
-  messagingSenderId: '621812643059',
-  appId: '1:621812643059:web:ec87d0f92b9addf424b386',
-  measurementId: 'G-T6MJJZN566',
-  databaseURL:'https://my-relationship-ai-1-default-rtdb.firebaseio.com/'
-
+  apiKey: 'AIzaSyDkvO7mK6MfeOW6Ozi9cIzEbMgE-QVFCRE',
+  authDomain: 'my-relationship-ai-7593b.firebaseapp.com',
+  projectId: 'my-relationship-ai-7593b',
+  storageBucket: 'my-relationship-ai-7593b.firebasestorage.app',
+  messagingSenderId: '447842556088',
+  appId: '1:447842556088:web:89a28915f09da2db4afc4d',
+  measurementId: 'G-0M7BC46GB1',
+  databaseURL:"https://my-relationship-ai-7593b-default-rtdb.firebaseio.com/",
 }
 
 
